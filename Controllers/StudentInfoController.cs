f485053c24d0df41cacbfa901e5ffb3bfc30e732
lenovo <lenovo@LAPTOP-BLNTEBH7>
2024-08-21 18:41:07 +0530

    this is the changes for webpack ado.net

--- Controllers/StudentInfoController.cs
+++ Controllers/StudentInfoController.cs
@@ -12,5 +12,55 @@ namespace WebApplicationNew_ADONET.Controllers
             var d = studentDataAccessLayer.GetAllStudent();
             return View(d);
         }
+
+        public IActionResult AddStudent()
+        {
+
+            return View();
+
+        }
+
+        public IActionResult Edit(int id)
+        {
+            StudentDataAccessLayer studentDataAccessLayer = new StudentDataAccessLayer();
+
+
+            var d = studentDataAccessLayer.GetStudentData(id);
+            return View("AddStudent",d);
+
+        }
+
+        public ActionResult Delete(int id)
+        {
+            StudentDataAccessLayer studentDataAccessLayer = new StudentDataAccessLayer();
+            studentDataAccessLayer.DeleteStudent(id);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(int id)
+        {
+            StudentDataAccessLayer studentDataAccessLayer = new StudentDataAccessLayer();
+            Student student = studentDataAccessLayer.GetStudentData(id);
+            return View(student);
+        }
+
+
+        [HttpPost]
+        public IActionResult AddStudent(Student student)
+        {
+            StudentDataAccessLayer studentDataAccessLayer = new StudentDataAccessLayer();
+            try
+            {
+                // TODO: Add insert logic here  
+                studentDataAccessLayer.AddStudent(student);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                return View();
+            }
+
+        }
     }
 }
